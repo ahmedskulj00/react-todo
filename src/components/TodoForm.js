d80c8035ff45cfba998fb409d7808488eb2b2f72
Ahmed <ahmedskulj10@gmail.com>
2021-04-17 00:30:04 +0200

    Work on TodoList component.

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -12,10 +12,10 @@ function TodoForm(props) {
         e.preventDefault()
 
 
-        // props.onSubmit({
-        //     id: Math.floor(Math.random() * 10000),
-        //     text: input
-        // })
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input
+        })
         
         setInput('')
 
